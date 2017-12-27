72242022c052eee314083d05d32d538cb6db9b15
AlessandroPozzi <pozzi.ale93@gmail.com>
2017-12-27 16:36:56 +0100

    Removed test.py, added exception class DataError

---
+++ src/DataError.py
@@ -0,0 +1,12 @@
+'''
+Created on 27 dic 2017
+
+@author: Alessandro Corsair
+'''
+
+class DataError(Exception):
+    '''
+    Custom exception to be used for any kind of data error while dealing with reads, write and operation on data in this project.
+    '''
+    pass
+        

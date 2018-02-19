c6d4644b985b3deba8470506d39eb45f6529eff1
LordCost93 <lorenzo1.costantini@mail.polimi.it>
2018-02-19 16:05:55 +0100

    Implementazione delle Markov chains

--- src/markovchain/expandDeviceMarkov.py
+++ src/markovchain/expandDeviceMarkov.py
@@ -17,6 +17,8 @@ id, LivelloPriorita
 import mysql.connector  # pip install mysql-connector-python
 # from helpers.File_writer import File_writer
 from File_writer import File_writer
+from ClusteringHandler import ClusterHandler
+from DataError import DataError
 import re
 import pomegranate as pomgr
 import graphviz as gv
@@ -27,17 +29,24 @@ import graphviz as gv
 import os
 
 
-def __create_txt(cursor, d):
-    # chosenDevices = ['EHS60/BE', 'EXS4/8X', 'EMC001*9', 'EXS106/2X', 'ESS1*84',
-    #                 'ESS11/5H', 'ESS406/E91', 'ESS407/E91', 'ESS520/E91', 'ESS11*84']
-    # our devices: ['EMC001*9', 'EHS60/BE', 'ESS11/5H', 'ESS1*84', 'EXS4/8X', 'EXS106/2X']
-    levelsOfPriority = ['L0', 'L1', 'L2', 'L3']
-    #chosenDevices = ['EMC001*9']
-    #levelsOfPriority = ['L1']
-    #consideredDevices = ['EMD1A*9', 'EMD2A*9', 'EMD3A*9', 'AUTO-TRANSFERT', 'ECC01/5DX', 'EMD700/1E', 'EMD701/1E']
+def create_sequences_txt(device):
+    cnx = mysql.connector.connect(host='127.0.0.1', user='root', password='password', database='cern')
+    cursor = cnx.cursor()
+    if "cluster" in config.FILE_SUFFIX:
+        __create_txt_clusters(cursor, device)
+    else:
+        __create_txt_noClusters(cursor, device)
 
-    #for d in chosenDevices:
-    #fw = File_writer(d, config.FILE_SUFFIX)
+def create_mc_model(device, priority, consideredDevices):
+    cnx = mysql.connector.connect(host='127.0.0.1', user='root', password='password', database='cern')
+    cursor = cnx.cursor()
+    mc = __get_markov_chain_model(cursor, device, priority, consideredDevices)
+    return mc
+
+
+def __create_txt_noClusters(cursor, d):
+    levelsOfPriority = ['L0', 'L1', 'L2', 'L3']
+
     fw = File_writer(d, "MC")
     fw.create_txt("../../res/")
     markedEvents = []
@@ -70,7 +79,7 @@ def __create_txt(cursor, d):
                 afterSeq.append(devicesAfter)  # Lista di liste (i.e. tutto quello dopo "distinct device after 5 min")
 
         # CONSOLE
-        print '\n\t\tDistinct devices after 5 minutes: [ '
+        print '\n\t\tSequences of activated devices after 5 minutes: [ '
         for xx in afterSeq:
             print '\t\t[ ',
             for yy in xx:
@@ -79,7 +88,7 @@ def __create_txt(cursor, d):
         print ']'
 
         # TEXT FILE
-        fw.write_txt('\n\t\tDistinct devices after 5 minutes: [ ')
+        fw.write_txt('\n\t\tSequences of activated devices after 5 minutes: [ ')
         for xx in afterSeq:
             fw.write_inline('\t\t[ ', )
             for yy in xx:
@@ -90,16 +99,86 @@ def __create_txt(cursor, d):
         print("==>")
         fw.write_txt('==>', newline=True)  # KEEP THIS!
 
-def create_sequences_txt(device):
-    cnx = mysql.connector.connect(host='127.0.0.1', user='root', password='password', database='cern')
-    cursor = cnx.cursor()
-    __create_txt(cursor, device)
 
-def create_mc_model(device, priority, consideredDevices):
-    cnx = mysql.connector.connect(host='127.0.0.1', user='root', password='password', database='cern')
-    cursor = cnx.cursor()
-    mc = __get_markov_chain_model(cursor, device, priority, consideredDevices)
-    return mc
+def __create_txt_clusters(cursor, d):
+
+    levelsOfPriority = ['L0', 'L1', 'L2', 'L3']
+
+    fw = File_writer(d, config.FILE_SUFFIX)
+    fw.create_txt("../../res/")
+    fw2 = File_writer(d, "DEBUG-" + config.FILE_SUFFIX)
+    fw2.create_txt("../../res/debug/")
+    print '\nDEVICE ' + str(d) + ': '
+    fw.write_txt('\nDEVICE ' + str(d) + ': ')
+    for l in levelsOfPriority:
+        print '\n\tPRIORITY ' + str(l) + ':'
+        fw.write_txt('\n\tPRIORITY ' + str(l) + ':')
+        query = (
+        "select * from electric where device=%s and livellopriorita=%s and action='Alarm CAME' order by time")
+        cursor.execute(query, (d, l))
+        events = cursor.fetchall()
+        clusterList = []
+
+        for e in events:
+            query = (
+            "select * from electric where time>=(%s) and time <= (%s + interval %s minute) and action='Alarm CAME' order by time;")
+            cursor.execute(query, (e[0], e[0], config.CORRELATION_MINUTES))
+            clusterHandler = ClusterHandler(e)  # Initialize the ClusterHandler with the reference event
+            eventsAfter = cursor.fetchall()
+
+            for ea in eventsAfter:
+                if ea != e:  # Ensures the reference event is not considered as a clustering point
+                    clusterHandler.addEvent(ea)
+
+            try:
+                # 1) OFFLINE AVERAGE
+                # clusterHandler.findClustersOfflineAverage(fw2, debug=True)
+                # 2) STATIC DISTANCE
+                # timeDelta = timedelta(seconds = 5)
+                # clusterHandler.findClustersStaticDistance(fw2, timeDelta, debug=True)
+                # 3) DBSCAN
+                # clusterHandler.findClustersDBSCAN(fw2, debug=True)
+                # 4) MEAN SHIFT
+                # clusterHandler.findClustersMeanShift(fw2, debug=True)
+                # 5) AVERAGE + STANDARD DEVIATION
+                clusterHandler.findClustersAverageDeviation(fw2, debug = True)
+
+            except DataError as e:
+                pass
+            newClusters = clusterHandler.getClusters()
+
+            # Create the list of itemsets
+            for stateList in newClusters:
+                devices = []
+                for evstate in stateList:
+                    devices.append(evstate.getDevice())
+                clusterList.append(devices)
+
+        # clusterList = a list of clusters, where each cluster is a list of devices
+        # stateList = a list of objects "EventState"
+        # evstate = a single object "EventState"
+
+        # uncomment this if you want to see the itemsets printed to the console
+        # CONSOLE
+        print '\n\t\tSequences of activated devices after 5 minutes: [ '
+        for devList in clusterList:
+            print '\t\t[ ',
+            for dev in devList:
+                print "'" + dev + "', ",
+            print '], '
+        print ']'
+
+        # TEXT FILE
+        fw.write_txt('\n\t\tSequences of activated devices after 5 minutes: [ ')
+        for devList in clusterList:
+            fw.write_inline('\t\t[ ', )
+            for dev in devList:
+                fw.write_inline("'" + dev + "', ")
+            fw.write_txt('], ')
+        fw.write_txt(']')
+
+        print("==>")
+        fw.write_txt('==>', newline=True)
 
 
 def __get_markov_chain_model(cursor, d, l, consideredDevices):
@@ -127,8 +206,7 @@ def __get_markov_chain_model(cursor, d, l, consideredDevices):
         for ea in eventsAfter:
             if ea not in markedEvents:  # CONDIZIONE per rimuovere i DUPLICATI
                 markedEvents.append(ea)
-                if ea[
-                    4] != d:  # CONDIZIONE per evitare problemi con il device di riferimento e l'aggiunta di stati, tag o descr.
+                if ea[4] != d:  # CONDIZIONE per evitare problemi con il device di riferimento e l'aggiunta di stati, tag o descr.
                     extraColumn = ea[22].encode('ascii', 'ignore').decode('ascii')
                     extraColumn.replace("'", "")
                     extraColumn = re.escape(extraColumn)
@@ -138,7 +216,7 @@ def __get_markov_chain_model(cursor, d, l, consideredDevices):
             afterSeq.append(devicesAfter)  # Lista di liste (i.e. tutto quello dopo "distinct device after 5 min")
 
     # CONSOLE
-    print '\n\t\tDistinct devices after 5 minutes: [ '
+    print '\n\t\tSequences of activated devices after 5 minutes: [ '
     for xx in afterSeq:
         print '\t\t[ ',
         for yy in xx:
